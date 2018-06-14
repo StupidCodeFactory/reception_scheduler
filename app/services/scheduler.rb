0fb2a868ca11a210b73e099bca8b13432ecab43a
yann marquet <ymarquet@gmail.com>
2018-06-14 18:27:44 +0100

    Scheduler
    * an employee can not work more than 40 hours
    * a shift can not be booked if not valid

--- app/services/scheduler.rb
+++ app/services/scheduler.rb
@@ -1,3 +1,20 @@
 class Scheduler
 
+  MAX_WORKING_HOURS = 40.hours.freeze
+
+  def initialize(establishment)
+    self.establishment = establishment
+  end
+
+  def book(employee, shift)
+    return false unless shift.valid?
+    return false if employee_would_be_over_capacity?(employee, shift)
+  end
+
+  private
+  attr_accessor :establishment
+
+  def employee_would_be_over_capacity?(employee, shift)
+    (shift.duration + employee.total_time_worked) > MAX_WORKING_HOURS
+  end
 end
--- spec/services/scheduler_spec.rb
+++ spec/services/scheduler_spec.rb
@@ -5,25 +5,31 @@ RSpec.describe Scheduler do
   subject { described_class.new(establishment) }
 
   describe '#book' do
-    let(:shift)    { double('shift', 'valid?': shift_validity) }
-    let(:employee) { double('employee', 'over_capacity?': employee_over_capacity) }
+
+    let(:shift)    { double('shift', 'valid?': shift_validity, duration: shift_duration) }
+    let(:shift_duration) { 6.hours }
+
+    let(:employee) { double('employee', total_time_worked: total_time_worked) }
+    let(:total_time_worked) { 0.hours }
 
     context 'when the shift is free' do
       let(:shift_validity) { true }
 
       context 'when the employee has not reached is working hours capacity' do
-        it { expect(subject.book(shift, employee)).to be true }
+        it { expect(subject.book(employee, shift)).to be true }
       end
 
       context 'when the employee has not reached is working hours capacity' do
-        it { expect(subject.book(shift, employee)).to be false }
+        let(:total_time_worked) { 40.hours }
+
+        it { expect(subject.book(employee, shift)).to be false }
       end
     end
 
     context 'when the shift is not free' do
       let(:shift_validity) { false }
 
-      it { expect(subject.book(shift, employee)).to be false }
+      it { expect(subject.book(employee, shift)).to be false }
     end
   end
 end
