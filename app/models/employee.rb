b25a511dd4c45be628056df3252beb640b2bd872
yann marquet <ymarquet@gmail.com>
2018-06-14 20:07:59 +0100

    weekly hours worked

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,7 @@
 class Employee < ApplicationRecord
+  has_many :shifts
+
+  def weekly_worked_hours_count
+    shifts.where('lower(slot) > ?', Time.zone.now.beginning_of_week).map(&:duration).sum
+  end
 end
--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Employee, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:establishment) { create :establishment }
+  subject { create :employee }
+
+  it { is_expected.to have_many(:shifts) }
+
+  describe '#weekly_worked_hours_count' do
+    let(:old_shift_start)   { Time.zone.now.beginning_of_week - 3.hours }
+    let!(:old_shift)        do
+      create(:shift,
+             slot: old_shift_start..(old_shift_start + 3.hours),
+             employee: subject,
+             establishment: establishment)
+    end
+
+    let!(:weekly_shift_one) do
+      create(:shift,
+             slot: 7.hours.ago..Time.zone.now,
+             employee: subject,
+             establishment: establishment
+            )
+    end
+    let!(:weekly_shift_two) do
+      create(:shift,
+             slot: 3.hours.ago..Time.zone.now,
+             employee: subject,
+             establishment: establishment)
+    end
+    before do
+      subject.reload
+    end
+
+    it { expect(subject.weekly_worked_hours_count).to eq(10.hours.to_i) }
+  end
 end
